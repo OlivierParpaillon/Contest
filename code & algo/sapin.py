fcf238bdde664a247dee5f19ee54e05fce068cb8
OlivierParpaillon <olivier.parpaillon@campus.academy>
2020-12-17 12:36:39 +0100

    code

--- code & algo/sapin.py
+++ code & algo/sapin.py
@@ -1,47 +1,35 @@
-# -*- coding:latin1 -*
-
-"""
-    Contest project 1 : Christmas Tree
-    Olivier PARPAILLON
-    Iliass RAMI
-    17/12/2020
-    python 3.7.7
-"""
-# Python program to generate branch christmas tree
-
-# Generating branch christmas tree
-
-
-def triangle_branch(n):
+def triangle_shape(n):
     for i in range(n):
         for j in range(n-i):
             print(' ', end='')
         for k in range(2*i+1):
             print('*', end='')
+        print()
 
 
 
-def triangle_branch2(n):
+
+def triangle_shape2(n):
     for i in range(n):
         for j in range(n-i):
             print(' ', end='')
         for k in range(4*i+3):
             print('*', end='')
+        print()
 
 
 
-def triangle_branch3(n):
+def triangle_shape3(n):
     for i in range(n):
         for j in range(n-i):
             print(' ', end='')
         for k in range(6*i+5):
             print('*', end='')
+        print()
 
 
-def tree_shape():
 
 
-
-# triangle_shape(4)
-# triangle_shape2(4)
-# triangle_shape3(4)
+triangle_shape(4)
+triangle_shape2(4)
+triangle_shape3(4)
