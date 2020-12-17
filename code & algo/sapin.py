98b7c6c907d42497707a2be44044601ff041f283
RIliass <rami.iliass.rami@gmail.com>
2020-12-17 11:39:13 +0100

    Premiere version du premier sapin

--- code & algo/sapin.py
+++ code & algo/sapin.py
@@ -1,11 +1,47 @@
-def print_segment(segment_size, total_width):
-    for line_size in range(1, segment_size + 1, 2):
-        print((line_size * "*").center(total_width))
+# -*- coding:latin1 -*
 
-def print_tree(size):
-    for segment_size in range(3, size + 1, 2):
-        print_segment(segment_size, size)
+"""
+    Contest project 1 : Christmas Tree
+    Olivier PARPAILLON
+    Iliass RAMI
+    17/12/2020
+    python 3.7.7
+"""
+# Python program to generate branch christmas tree
 
-print("Choisissez la taille de votre Arbre de Noël :")
-tree_size = int(input())
-print_tree(tree_size)
+# Generating branch christmas tree
+
+
+def triangle_branch(n):
+    for i in range(n):
+        for j in range(n-i):
+            print(' ', end='')
+        for k in range(2*i+1):
+            print('*', end='')
+
+
+
+def triangle_branch2(n):
+    for i in range(n):
+        for j in range(n-i):
+            print(' ', end='')
+        for k in range(4*i+3):
+            print('*', end='')
+
+
+
+def triangle_branch3(n):
+    for i in range(n):
+        for j in range(n-i):
+            print(' ', end='')
+        for k in range(6*i+5):
+            print('*', end='')
+
+
+def tree_shape():
+
+
+
+# triangle_shape(4)
+# triangle_shape2(4)
+# triangle_shape3(4)
